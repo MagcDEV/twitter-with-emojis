a5a9622aa30c7dde36b1b8a6057924526b925fd6
Manuel Guevara <manuel117g@gmail.com>
2023-05-02 11:57:00 -0400

    Update filterUserForClient.ts

--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -5,6 +5,5 @@ export const filterUserForClient = (user: User) => {
     id: user.id,
     username: user.username,
     profilePicture: user.profileImageUrl,
-    externalUsername: user.externalAccounts.find((externalAccount) => externalAccount.provider === "oauth_github")?.username || null
   };
 };
